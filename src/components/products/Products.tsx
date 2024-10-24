92766607c6b1c5f023d940f990a4b1d54976b426
Edgar Macharia <m.edgarmacharia@gmail.com>
2024-10-24 21:08:27 +0300

    add test data for products

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -1,8 +1,33 @@
+import Product from "./Product";
 
+
+interface ProductType {
+  id: number;
+  name: string;
+  description: string;
+  price: string;
+}
+
+
+const products: ProductType[] = [
+  { id: 1, name: 'Shoes', description: 'Running shoes', price: '$10' },
+  { id: 2, name: 'Macbook', description: 'Apple Macbook', price: '$5' },
+  {id: 3, name: 'Phone', description: 'Running shoes', price: '$10'},
+  {id: 4, name: 'Headphones', description: 'Running shoes', price: '$10'}
+]
 
 const Products = () => {
+
   return (
-    <div>Products</div>
+    <div className="my-4">
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mx-2">
+      {products.map((product) => (
+        <div key={product.id} className="col-span-1">
+          <Product product={product} />
+        </div>
+      ))}
+      </div>
+    </div>
   )
 }
 
