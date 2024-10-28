e2ae1082411afa90485bf739316f747d4421ae58
Edgar Macharia <m.edgarmacharia@gmail.com>
2024-10-28 22:02:55 +0300

    add navlinks

---
+++ src/constants/index.tsx
@@ -0,0 +1,26 @@
+export const navLinks = [
+    {
+        id: "Shop",
+        title: "Shop",
+    },
+    {
+        id: "Checkout",
+        title: "Checkout",
+    },
+    {
+        id: "Orders",
+        title: "My Orders",
+    },
+    {
+        id: "Categories",
+        title: "Categories",
+    },
+    {
+        id: "Account",
+        title: "Account",
+    },
+    {
+        id: "Wishlist",
+        title: "Wishlist",
+    }
+]
