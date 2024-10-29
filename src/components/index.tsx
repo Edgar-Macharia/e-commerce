0cfa5e2f9a8fbc27a7379a7aaa7597e3ed0fbe5d
Edgar Macharia <m.edgarmacharia@gmail.com>
2024-10-29 17:00:03 +0300

    export breadcrumb

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,4 @@
 export { default as Products } from './products/Products';
 export { default as Navbar } from './Navbar';
 export { default as Search } from './Search';
+export { default as Breadcrumb } from './Breadcrumb';
