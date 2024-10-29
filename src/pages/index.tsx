795df34be70848533e6d1b55662d6ed3003a8543
Edgar Macharia <m.edgarmacharia@gmail.com>
2024-10-29 15:37:39 +0300

    add pages

---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+export { default as Home } from './Home';
+export { default as Cart } from './Cart';
+export { default as Checkout } from './Checkout';
+export { default as Orders } from './Orders';
+export { default as Shop } from './Shop';
