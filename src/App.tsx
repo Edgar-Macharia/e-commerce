cb8e51cbed7ceead608a73ac2c3498090c5eddd9
Edgar Macharia <m.edgarmacharia@gmail.com>
2024-10-29 15:39:43 +0300

    add app routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import { Products } from './components';
+import { Routes, Route } from 'react-router-dom';
+import { Navbar } from './components';
+import Layout from './layout/Layout';
+import { Home, Shop, Cart, Checkout, Orders} from './pages'
 
 
 const App = () => {
   return (
-    <div className='container font-display text-primary overflow-hidden'>
-      <Products />
-    </div>
+    <>
+      <div className='font-display text-primary'>
+        <Navbar />
+      </div>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='/shop' element={<Shop />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/checkout' element={<Checkout />} />
+          <Route path='/my-orders' element={<Orders />} />
+        </Route>
+      </Routes>
+    </>
   )
 }
 
