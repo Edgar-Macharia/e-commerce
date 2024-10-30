5d8af1b7d3af73151aab6cc9e52e77b147ac303d
Edgar Macharia <m.edgarmacharia@gmail.com>
2024-10-30 17:28:02 +0300

    add product route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import { Navbar } from './components';
 import Layout from './layout/Layout';
-import { Home, Shop, Cart, Checkout, Orders} from './pages'
+import { Home, Shop, Product, Cart, Checkout, Orders} from './pages'
 
 
 const App = () => {
@@ -14,6 +14,7 @@ const App = () => {
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
           <Route path='/shop' element={<Shop />} />
+          <Route path='product/:id' element={<Product />} />
           <Route path='/cart' element={<Cart />} />
           <Route path='/checkout' element={<Checkout />} />
           <Route path='/my-orders' element={<Orders />} />
