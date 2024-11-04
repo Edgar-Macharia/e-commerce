6564af6519e202733802274ec5694929c9862cd0
Edgar Macharia <m.edgarmacharia@gmail.com>
2024-11-04 21:34:12 +0300

    export cart component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,5 @@
 export { default as Products } from './products/Products';
 export { default as Navbar } from './Navbar';
+export { default as Cart } from './Cart';
 export { default as Search } from './Search';
 export { default as Breadcrumb } from './Breadcrumb';
